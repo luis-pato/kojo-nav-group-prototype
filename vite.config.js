21cec6c8f7631a2f3767c1b34c64f4fb808e3252
LuisPato <lpato@dfau.de>
2024-10-22 15:00:57 +0200

    change base on the vite config so we can deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 export default defineConfig({
+    base: '/dist/',
     plugins: [
         ViteEjsPlugin()
     ],
